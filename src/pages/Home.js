d2fd5df7452d3b1e0d2d12cc51b6f3767a465766
anandpothraj <anandpothraj11052001@gmail.com>
2023-01-15 22:45:07 +0530

    Created Work Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Profile from '../components/Home/Profile';
+import Work from "../components/Home/Work";
 
 const Home = () => {
   return (
     <div className="bg-dark">
       <Profile/>
+      <Work/>
     </div>
   );
 };
-
 export default Home;
